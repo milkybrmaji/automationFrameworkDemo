69bb039b01952525eb01d3e656e8ddf5974dd013
Milky Brmaji <57339047+milkybrmaji@users.noreply.github.com>
2023-12-14 08:17:21 -0800

    Update salesSummaryPage.py
    Sales summary page object still needs to be updated. Not finished yet

--- pageObjects/salesSummaryPage.py
+++ pageObjects/salesSummaryPage.py
@@ -1,1 +1,12 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys 
 
+class searchSalesSummary():
+  lnk_CustomersMenu_Xpath =  "//a[@href='#']//p[contains(text(),'Reports')]"
+  lnk_CustomersPage_Xpath = "//a[@href='/Admin/Report/SalesSummary']//p[contains(text(),'Sales summary')]"
+
+  txtInput_StartDate_Id = "StartDate"
+  txtInput_EndDate_Id = "EndDate"
+  
