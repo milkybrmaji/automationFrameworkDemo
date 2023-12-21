fd8f4e69223ed2436690e3e1eea0aaa8502ee802
Milky Brmaji <importantmilky@gmail.com>
2023-12-21 11:14:18 -0800

    updated this file while there are new updated in the master branch

--- dummyfile.py
+++ dummyfile.py
@@ -1,2 +1,9 @@
 # This is a dummy file to do some git pull, push, merge testings
 # I'm going to pull this updated master branch to my local repository 
+
+#I've pulled the updated master branch successfully and now my local repository is up to date
+#I've added and commited this comment section to my local git repository version control and I'm now going to push it
+#but i've also committed new changes to this file from github. I should get an error message when
+#trying to push this file into the master branch, and the error message should request that I pull from
+#master branch first to get my local repository up to date with the master branch on github. Then I will
+#be able to push the new commits from my local repository and merge it with the master branch.
